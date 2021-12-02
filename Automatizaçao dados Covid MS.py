9d8d6f5b1a204335d3f89e3b24e0e4b3c8a391cf
Diego Lima <83326137+diegofdsl@users.noreply.github.com>
2021-12-02 14:45:57 -0300

    Update Automatizaçao dados Covid MS.py

--- Automatizaçao dados Covid MS.py
+++ Automatizaçao dados Covid MS.py
@@ -6,6 +6,9 @@ from time import sleep
 navegador= webdriver.Chrome()
 navegador.get('https://covid.saude.gov.br/')
 navegador.find_element_by_xpath('/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/div[1]/div[2]/ion-button').click()
+#Tempo de espera em segundos para o download, caso sua internet seja rápida pode diminuir para otimizar o tempo
 sleep(60)
+
+
 navegador.close()
 
